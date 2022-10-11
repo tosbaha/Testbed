135d671100db3aa434914b801a19e87d0e9f1bb0
Mustafa <971530+tosbaha@users.noreply.github.com>
2022-10-11 10:28:02 +0300

    Initial commit

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '15.0'
+
+target 'Testbed' do
+  pod 'Eureka'	
+  pod 'SwiftGen'
+end
---
+++ Testbed/TestbedApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestbedApp.swift
+//  Testbed
+//
+//  Created by Mustafa on 11.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct TestbedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
